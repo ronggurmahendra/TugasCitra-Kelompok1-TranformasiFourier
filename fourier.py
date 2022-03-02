551a65df4b02ebbb92b24a6b269b8a9fc6b25452
Ronggur Mahendra Widya Putra <ronggur.mwp@gmail.com>
2022-03-02 14:28:27 +0700

    initial commit

---
+++ fourier.py
@@ -0,0 +1,72 @@
+import cv2 as cv
+import numpy as np 
+from matplotlib import pyplot as plt
+
+from skimage.io import imread, imshow
+from skimage.color import rgb2hsv, rgb2gray, rgb2yuv
+from skimage import color, exposure, transform
+from skimage.exposure import equalize_hist
+
+from os import environ
+
+def suppress_qt_warnings():
+    environ["QT_DEVICE_PIXEL_RATIO"] = "0"
+    environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
+    environ["QT_SCREEN_SCALE_FACTORS"] = "1"
+    environ["QT_SCALE_FACTOR"] = "1"
+
+if __name__ == "__main__":
+    suppress_qt_warnings()
+
+    img = cv.imread('./img.png',0)
+    f = np.fft.fft2(img)
+    fshift = np.fft.fftshift(f)
+    magnitude_spectrum = 20*np.log(np.abs(fshift))
+    plt.subplot(121),plt.imshow(img, cmap = 'gray')
+    plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
+    plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+    plt.show()
+
+    rows, cols = img.shape
+    crow,ccol = rows//2 , cols//2
+    fshift[crow-30:crow+31, ccol-30:ccol+31] = 0
+    f_ishift = np.fft.ifftshift(fshift)
+    img_back = np.fft.ifft2(f_ishift)
+    img_back = np.real(img_back)
+    plt.subplot(131),plt.imshow(img, cmap = 'gray')
+    plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(132),plt.imshow(img_back, cmap = 'gray')
+    plt.title('Image after HPF'), plt.xticks([]), plt.yticks([])
+    plt.subplot(133),plt.imshow(img_back)
+    plt.title('Result in JET'), plt.xticks([]), plt.yticks([])
+    plt.show()
+
+    img = cv.imread('./img.png',0)
+    dft = cv.dft(np.float32(img),flags = cv.DFT_COMPLEX_OUTPUT)
+    dft_shift = np.fft.fftshift(dft)
+    magnitude_spectrum = 20*np.log(cv.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
+    plt.subplot(121),plt.imshow(img, cmap = 'gray')
+    plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
+    plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+    plt.show()
+
+    rows, cols = img.shape
+    crow,ccol = rows/2 , cols/2
+    crow = int(crow)
+    ccol = int(ccol)
+    # create a mask first, center square is 1, remaining all zeros
+    mask = np.zeros((rows,cols,2),np.uint8)
+    mask[crow-30:crow+30, ccol-30:ccol+30] = 1
+
+    # apply mask and inverse DFT
+    fshift = dft_shift*mask
+    f_ishift = np.fft.ifftshift(fshift)
+    img_back = cv.idft(f_ishift)
+    img_back = cv.magnitude(img_back[:,:,0],img_back[:,:,1])
+    plt.subplot(121),plt.imshow(img, cmap = 'gray')
+    plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(img_back, cmap = 'gray')
+    plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+    plt.show()
